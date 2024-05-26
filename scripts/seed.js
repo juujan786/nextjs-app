f4e40a28de0f97b2cb97bdf0e80377cda343ac98
Javed Hussain <jhsurmovi@gmail.com>
2024-05-27 00:31:28 +0500

    second commit

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,4 +1,5 @@
-const { db } = require('@vercel/postgres');
+require('dotenv').config();
+const { db, createClient } = require('@vercel/postgres');
 const {
   invoices,
   customers,
@@ -161,7 +162,9 @@ async function seedRevenue(client) {
 }
 
 async function main() {
+
   const client = await db.connect();
+  // const client = createClient();
 
   await seedUsers(client);
   await seedCustomers(client);
